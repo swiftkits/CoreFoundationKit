860310b91584163f231f204f2d7b717ac58d04d5
0vertex <>
2021-10-17 21:22:24 +0530

    Added initial setups for project.

---
+++ Sources/CoreFoundationKit.h
@@ -0,0 +1,18 @@
+//
+//  CoreFoundationKit.h
+//  CoreFoundationKit
+//
+//  Created by Manish on 17/10/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoreFoundationKit.
+FOUNDATION_EXPORT double CoreFoundationKitVersionNumber;
+
+//! Project version string for CoreFoundationKit.
+FOUNDATION_EXPORT const unsigned char CoreFoundationKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreFoundationKit/PublicHeader.h>
+
+
